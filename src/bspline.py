dbf0c1e004e79921505b1e0cf370170e0e4631c0
TanookiVerde <pedrovitor.marques@gmail.com>
2017-11-16 18:30:50 -0200

    Adicionado A* Pathfinding

--- src/bspline.py
+++ src/bspline.py
@@ -1,50 +1,41 @@
 def Interpolate(t,degree,points,knots,weights): 
-    i,j,s,m = 0,0,0,0
     n = len(points)
     dim = len(points[0])
-    
     dom = [degree,len(knots)-1-degree]
-    
     low = knots[dom[0]]
     high = knots[dom[1]]
     t = t*(high-low)+low
-
-    if t < low or t > high:
-        print("Index t muito grande ou muito pequeno")
-
     #Acha o segmento certo
     for s in range(dom[0],dom[1]):
         if t >= knots[s] and t<=knots[s+1]:
             break
-    
     #Convertendo para coordenadas homogeneas
     v = [[0 for x in range(0,dim+1)] for y in range(0,n)]
     for i in range(0,n):
         for j in range(0,dim):
             v[i][j] = points[i][j]*weights[i]
         v[i][dim] = weights[i]
-
     #Interpola pontos 
     alpha = 1
     for m in range(1,degree+1):
         for i in range(s,s-degree-1+m,-1):
             alpha = (t-knots[i])/(knots[i+degree+1-m]-knots[i])
-            for j in range(0,d+1):
+            for j in range(0,dim+1):
                 v[i][j]=(1-alpha)*v[i-1][j]+alpha*v[i][j]
-
     #Converte de volta para coordenadas cartesianas
     result = [0 for x in range(0,dim+1)]
     for j in range(0,dim):
         result[j] = v[s][j]/v[s][dim]
-
     return result
 
-p = [[50,50],[100,50],[150,50],[150,100],[150,150],[100,150]]
-k = [0,1,2,3,4,5,6,7,8] #len(k) = len(p) + d + 1
+#ZONA DE TESTES
+"""
+p = [[50,50],[100,50],[150,100],[150,150],[100,150],[50,100]]
+k = [1,1,1,2,3,4,5,5,5] #len(k) = len(p) + d + 1
 w = [1,1,1,1,1,1] # len(w) = len(p)
 d = 2
 t = 0
-"""
+
 for i in range(0,100,1):
     print(Interpolate(i/100,d,p,k,w))
 """
--- src/main.py
+++ src/main.py
@@ -1,29 +1,50 @@
 from bspline import*
+from grid import*
 import pygame
 import sys
 from pygame.locals import*
 
+S_WIDTH,S_HEIGHT = 500,500
 pygame.init()
 
-DISPLAYSURF = pygame.display.set_mode((400, 300))
+DISPLAYSURF = pygame.display.set_mode((S_WIDTH, S_HEIGHT))
 pygame.display.set_caption('Hello World!')
 
 def DrawPoints(points):
     color = (255,0,0)
     for i in range(0,len(points)):
         pygame.draw.circle(DISPLAYSURF,color,points[i],5,0)
-def DrawPath():
+def DrawPath(points):
     for i in range(0,100):
-        ponto = Interpolate(i/100.0,d,p,k,w) 
+        ponto = Interpolate(i/100.0,d,points,k,w) 
         temp = [int(ponto[0]),int(ponto[1])]
         pygame.draw.circle(DISPLAYSURF,(0,255,0),temp,2,0)
+
+#ZONA DE TESTES
+wi = [[1 for i in range(10)] for j in range(10)]
+g = SquareGrid(10,10,wi)
+start = (1,1)
+goal = (7,8)
+r = a_star_search(g,start,goal)
+k = [1,1,1,2,3,4,5,6,7,8,9,10,11,12,12,12] #len(k) = len(p) + d + 1
+w = [1,1,1,1,1,1,1,1,1,1,1,1,1] # len(w) = len(p)
+d = 2
+t = 0
+v = [[0 for x in range(0,2)] for y in range(0,13)]
+for i in range(0,10):
+    for j in range(0,2):
+        v[i][j] = r[i][j]*50
+        
+#NAO MEXE
+DISPLAYSURF.fill((255,255,255))
+DrawPath(v)
+DrawPoints(v)
+
+#MAIN LOOP
 while True:
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
             sys.exit()
     pygame.display.update()
-    DISPLAYSURF.fill((255,255,255))
-    DrawPath()
-    DrawPoints(p)
 
